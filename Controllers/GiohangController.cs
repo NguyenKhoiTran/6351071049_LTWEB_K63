bfcf8b0239aebfd760debb30a2b63a0c8d5a1e19
unknown <trankhoinguyen264@gmail.com>
2024-11-08 16:19:59 +0700

    buoi 2

---
+++ Controllers/GiohangController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH1.Models;
+
+namespace TH1.Controllers
+{
+    public class GiohangController : Controller
+    {
+        // GET: Giohang
+        QLBANSACHEntities data = new QLBANSACHEntities();
+
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"]= lstGiohang;
+            }
+            return lstGiohang;
+        }
+        // Thêm hàng vào giỏ
+        public ActionResult ThemGiohang(int iMasach, string strURL)
+        {
+            // Lấy ra Session giỏ hàng
+            List<Giohang> lstGiohang = Laygiohang();
+
+            // Kiểm tra sách này tồn tại trong Session["Giohang"] chưa?
+            Giohang sanpham = lstGiohang.Find(n => n.iMasach == iMasach);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(iMasach);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        // Cập nhật phương thức tính tổng số lượng
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongSoLuong;
+        }
+
+        // Cập nhật phương thức tính Tongtien()
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongTien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongTien;
+        }
+
+        // Cập nhật phương thức xử lý hiển thị Giỏ hàng : GioHang()
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return GiohangPartial();
+        }
+        public ActionResult XoaGiohang(int iMaSP)
+        {
+            // Lay gio hang tu Session
+            List<Giohang> lstGiohang = Laygiohang();
+            // Kiem tra sach da co trong Session["Giohang"]
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMasach == iMaSP);
+            // Neu ton tai thi cho sua Soluong
+            if (sanpham != null)
+            {
+                lstGiohang.RemoveAll(n => n.iMasach == iMaSP);
+                return RedirectToAction("GioHang");
+            }
+
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f) 
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMasach == iMaSP);
+            if(sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(f["txtSoluong"].ToString());
+            }
+            return RedirectToAction("Giohang");
+        }
+        //Hiển thị View DatHang để cập nhật các thông tin cho đơn hàng
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            //Kiểm tra đăng nhập
+            if (Session["Taikhoan"] == null || Session["Taikhoan"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "Nguoidung");
+            }
+
+            //Kiểm tra giỏ hàng
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index","BookStore");
+            }
+
+            //Lấy giỏ hàng từ Session
+            List<Giohang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+
+            return View(lstGiohang);
+        }
+        public ActionResult DatHang (FormCollection collection)
+        {
+            DONDATHANG ddh = new DONDATHANG();
+            KHACHHANG kh = (KHACHHANG)Session["Taikhoan"];
+            List<Giohang> gh = Laygiohang();
+            ddh.MaKH = kh.MaKH;
+            ddh.Ngaydat = DateTime.Now;
+            var ngaygiao = String.Format("{0:MM/dd/yyyy}", collection["Ngaygiao"]);
+            ddh.Ngaygiao =DateTime.Parse(ngaygiao);
+            ddh.Tinhtranggiaohang = false;
+            ddh.Dathanhtoan = false;
+            data.DONDATHANGs.Add(ddh);
+            data.SaveChanges();
+
+            foreach(var item in gh)
+            {
+                CHITIETDONTHANG ctdh = new CHITIETDONTHANG();
+                ctdh.MaDonHang =ddh.MaDonHang;
+                ctdh.Masach = item.iMasach;
+                ctdh.Soluong = item.iSoluong;
+                ctdh.Dongia = (decimal) item.dDongia;
+                data.CHITIETDONTHANGs.Add(ctdh);
+            }
+            data.SaveChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("Xacnhandonhang", "Giohang");
+        }
+        public ActionResult Xacnhandonhang()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Giohang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TH1.Models
+{
+    public class Giohang
+    {
+        QLBANSACHEntities data = new QLBANSACHEntities();
+
+        public int iMasach { set; get; }
+        public string sTensach { set; get; }
+        public string sAnhbia { set; get; }
+        public double dDongia { set; get; }
+        public int iSoluong { set; get; }
+
+        public double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+
+        // Khởi tạo giỏ hàng theo Masach được truyền vào với Soluong mặc định là 1
+        public Giohang(int Masach)
+        {
+            iMasach = Masach;
+            SACH sach = data.SACHes.Single(n => n.Masach == iMasach);
+            sTensach = sach.Tensach;
+            sAnhbia = sach.Anhbia;
+            dDongia = double.Parse(sach.Giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
